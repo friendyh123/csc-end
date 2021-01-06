ee663cc7ecb5dcc0a09b0ca2b38c5502031654b0
friendyh123 <friendyh@qq.com>
2021-01-06 11:05:05 +0800

    代码优化

--- src/main/java/com/gfzq/csc_end/cscend.java
+++ src/main/java/com/gfzq/csc_end/cscend.java
@@ -10,7 +10,6 @@ public class cscend {
 	
 	public static void main(String[] args) {
 		SqlSessionFactory factory = sessionFactory.getSessionFactory();		
-		WebSocketImpl.DEBUG = true;
 	    int port = 8887; // 端口
 	    WsServer s = new WsServer(port,factory);
 	    s.start();
